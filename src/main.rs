114faf00c94b669a963d7165f45123560132f721
rohibhan <jainrohit090@gmail.com>
2024-03-14 14:34:04 +0530

    commit from imac-added trait

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+#[derive(Debug)]
+struct Person {
+    name: String,
+    age: u8,
+}
+
+trait Speak {
+    fn say_hello(&self) -> String;
+}
+
+impl Speak for Person {
+    fn say_hello(&self) -> String {
+        format!("Hello, My name is {} and I am {} years old", self.name, self.age)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let person = Person {
+        name: String::from("Jinnal Parakh"),
+        age: 29,
+    };
+    println!("{:?}", person.say_hello());
 }
